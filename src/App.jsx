ce141fbbf830e84684d972aaf98771bd7b8e0afc
Mustafiz <mustafizansari35@gmail.com>
2025-03-07 21:45:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Codilex Ai</h1>
+      <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
