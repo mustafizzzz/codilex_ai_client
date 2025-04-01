698b4abf271300168567cd973ac295412d383a02
Mustafiz <mustafizansari35@gmail.com>
2025-04-02 00:25:29 +0530

    Remove unnecessary blank line in App component for cleaner code structure

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,6 @@ function App() {
       <Router>
         <div className="flex flex-col min-h-screen">
           <Navbar />
-
           <main className='flex-grow'>
             <Routes>
               <Route path="/" element={<HomePage />} />
